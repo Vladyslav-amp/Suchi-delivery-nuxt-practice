0bf6a33da5a8249ce91bf511bc093ed120f6f2ee
Vladorius <lutsyk710@gmail.com>
2024-06-05 14:19:08 +0200

    Updates

---
+++ assets/_id_.fc3e3cfb.js
@@ -0,0 +1,1 @@
+import{a as c,b as t,f as n,e as l,h as i,t as e,u as a,i as d,o as p}from"./entry.383eb23c.js";const B={__name:"[id]",setup(f){const s=c(),r=t("stateProducts"),o=n.filterArrayByKey(r.value,"slug",s.params.id),u=t("stateCartQuantity");return(_,m)=>(p(),l("div",null,[i(e(a(u))+" ",1),d("p",null,e(a(o)[0].title),1)]))}};export{B as default};
